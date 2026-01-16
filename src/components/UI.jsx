b31db26ae1f47b2d4781bc0a73237bdb2c95127e
Monsur Raji <monsurrajiybs@gmail.com>
2026-01-16 09:19:20 +0100

    ss

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -161,7 +161,7 @@ const isValidEmail = (email) => {
     <div className="bg-white/0 animate-horizontal-scroll flex items-center gap-8 w-max px-8">
       
       <h1 className="shrink-0 text-white text-10xl font-black ">
-        Cooing Of A Homing Peagen 
+        Cooing Of A Homing Pigeon 
       </h1>
 
       <h2 className="shrink-0 text-white text-8xl italic font-light">
@@ -173,7 +173,7 @@ const isValidEmail = (email) => {
       </h2>
 
       <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-        Cooing Of A Homing Peagen 
+        Cooing Of A Homing Pigeon 
       </h2>
 
       <h2 className="shrink-0 text-white text-9xl font-medium">
@@ -185,7 +185,7 @@ const isValidEmail = (email) => {
       </h2>
 
       <h2 className="shrink-0 text-white text-13xl font-bold">
-        Cooing Of A Homing Peagen
+        Cooing Of A Homing Pigeon
       </h2>
 
       <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
@@ -197,7 +197,7 @@ const isValidEmail = (email) => {
     <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
       
       <h1 className="shrink-0 text-white text-10xl font-black ">
-        Cooing Of A Homing Peagen
+        Cooing Of A Homing Pigeon
       </h1>
 
       <h2 className="shrink-0 text-white text-8xl italic font-light">
@@ -209,7 +209,7 @@ const isValidEmail = (email) => {
       </h2>
 
       <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-        Cooing Of A Homing Peagen
+        Cooing Of A Homing Pigeon
       </h2>
 
       <h2 className="shrink-0 text-white text-9xl font-medium">
@@ -221,7 +221,7 @@ const isValidEmail = (email) => {
       </h2>
 
       <h2 className="shrink-0 text-white text-13xl font-bold">
-        Cooing Of A Homing Peagen
+        Cooing Of A Homing Pigeon
       </h2>
 
       <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
